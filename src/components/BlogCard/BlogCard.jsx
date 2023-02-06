29e2d2d253814f1b17792a0dbc7010f7f1d4a632
Lina Budkova <linabudkova35@gmail.com>
2023-02-06 15:02:36 +0200

    add task-1

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 import {
   Card,
   CardBody,
@@ -14,6 +16,44 @@ import {
   Date,
 } from './BlogCard.styled';
 
-export const BlogCard = () => {
-  return <div>BlogCard</div>;
+export const BlogCard = ({
+  name,
+  postedAt,
+  poster,
+  tag,
+  title,
+  description,
+  avatar,
+}) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardPoster src={poster} alt={tag} />
+      </CardHeader>
+      <CardBody>
+        <Tag>{tag}</Tag>
+        <CardTitle>{title}</CardTitle>
+        <CardText>{description}</CardText>
+      </CardBody>
+      <CardFooter>
+        <UserBox>
+          <Avatar src={avatar} alt={name} />
+          <UserInfo>
+            <UserName>{name}</UserName>
+            <Date>{postedAt}</Date>
+          </UserInfo>
+        </UserBox>
+      </CardFooter>
+    </Card>
+  );
+};
+
+BlogCard.propTypes = {
+  poster: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  postedAt: PropTypes.string.isRequired,
 };
