c2be5f6efb4605dd6f7e84ea5fd7804b9ff490a0
Iryna-Tyshchuk <ukriris93@gmail.com>
2023-02-06 15:49:20 +0200

    task-2

--- src/components/StatisticItem/StatisticItem.jsx
+++ src/components/StatisticItem/StatisticItem.jsx
@@ -1,9 +1,20 @@
+import PropTypes from 'prop-types';
 import {
   StatisticBox,
   StatisticText,
   StatisticCounter,
 } from './StatisticItem.styled';
 
-export const StatisticItem = () => {
-  return <div>StatisticBox</div>;
+export const StatisticItem = ({ title, total }) => {
+  return (
+    <StatisticBox>
+      {/* Тут має бути іконка */}
+      <StatisticCounter>{total}</StatisticCounter>
+      <StatisticText>{title}</StatisticText>
+    </StatisticBox>
+  );
+};
+StatisticItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  total: PropTypes.number.isRequired,
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,13 +1,31 @@
+import PropTypes from 'prop-types';
+
 import { StatisticItem } from 'components';
 import { StatisticsList, StatisticTitle } from './Statistics.styled';
 import { FaRegThumbsUp } from 'react-icons/fa';
 import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
 import { GiTreeDoor } from 'react-icons/gi';
 
-export const Statistics = () => {
+export const Statistics = ({ title, stats }) => {
   return (
     <>
-      <div>StatisticsList</div>
+      {title && <StatisticTitle>{title}</StatisticTitle>}
+
+      <StatisticsList>
+        {stats.map(({ id, title, total }) => {
+          return <StatisticItem key={id} title={title} total={total} />;
+        })}
+      </StatisticsList>
     </>
   );
+};
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      total: PropTypes.number.isRequired,
+    }).isRequired,
+  ).isRequired,
 };
