c3a6d62be2fe490eef1401b5c3836a41161bd1db
Lina Budkova <linabudkova35@gmail.com>
2023-02-08 14:43:56 +0200

    add form to task 2

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,36 @@ import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
 export class SearchForm extends Component {
+  state = {
+    todo: '',
+  };
+
+  handleChange = e => {
+    this.setState({ todo: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    // console.log(this.state);
+    this.props.getTodo({ ...this.state });
+    this.setState({ todo: '' });
+  };
+
   render() {
-    return <h2>SearchForm</h2>;
+    return (
+      <SearchFormStyled onSubmit={this.handleSubmit}>
+        <FormBtn type="submit">
+          <FiSearch size="16px" />
+        </FormBtn>
+        <InputSearch
+          placeholder="What do you want to write?"
+          name="search"
+          required
+          autoFocus
+          onChange={this.handleChange}
+          value={this.state.todo}
+        />
+      </SearchFormStyled>
+    );
   }
 }
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -4,6 +4,10 @@ import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 
 export class Todos extends Component {
   render() {
-    return <Text>Todos</Text>;
+    return (
+      <>
+        <SearchForm getTodo={() => {}} />
+      </>
+    );
   }
 }
