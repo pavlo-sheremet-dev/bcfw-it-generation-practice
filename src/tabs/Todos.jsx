ce6c22189a4194beff027f2df692f3aa0f435d31
Iryna-Tyshchuk <ukriris93@gmail.com>
2023-02-08 15:27:02 +0200

    Update Todos.jsx

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -3,10 +3,32 @@ import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 
 export class Todos extends Component {
+  state = {
+    todos: [],
+  };
+  addTodo = newTodo => {
+    const todo = { ...newTodo, id: nanoid() };
+    this.setState(prevState => ({ todos: [...prevState.todos, todo] }));
+  };
+  deleteTodo = id => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== id),
+    }));
+  };
   render() {
+    const { todos } = this.state;
     return (
       <>
-        <SearchForm getTodo={() => {}} />
+        <SearchForm getTodo={this.addTodo} />
+        {todos.length !== 0 ? (
+          <Grid>
+            <GridItem>
+              <Todo />
+            </GridItem>
+          </Grid>
+        ) : (
+          <Text>there are no any todos</Text>
+        )}
       </>
     );
   }
