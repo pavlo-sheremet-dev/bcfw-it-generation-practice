330d87f29149785732b5d0594bcd64e755ededab
Lina Budkova <linabudkova35@gmail.com>
2023-02-14 15:36:18 +0200

    add gallery

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -3,6 +3,8 @@ import { Component } from 'react';
 import * as api from 'service/image-service';
 
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export class Gallery extends Component {
   state = {
@@ -14,27 +16,65 @@ export class Gallery extends Component {
     error: '',
   };
 
-  componentDidMount() {
-    this.getImages();
+  // componentDidMount() {
+  //   this.getImages();
+  // }
+  componentDidUpdate(_, prevState) {
+    const { query, page, error } = this.state;
+    if (prevState.query !== query || prevState.page !== page) {
+      this.getImages();
+    }
+    if (prevState.error !== error && error) {
+      toast.error(error);
+    }
   }
 
   getImages = async () => {
+    const { query, page } = this.state;
     try {
-      const { photos } = await api.getImages('cat', 1);
+      this.setState({ loading: true });
+      const { photos, total_results } = await api.getImages(query, page);
+      if (!photos.length) {
+        this.setState({ error: 'Sorry. There are no images ... 😭' });
+        return;
+      }
       this.setState(prevState => ({
         images: [...prevState.images, ...photos],
+        error: '',
+        totalImages: total_results,
       }));
     } catch (error) {
-      console.log(error);
+      this.setState({ error: 'Oops. Something went wrong 😭' });
+    } finally {
+      this.setState({ loading: false });
     }
   };
 
+  getQuery = ({ value }) => {
+    if (!value.trim() || value === this.state.query) {
+      this.setState({ error: 'Please, change your request' });
+      return;
+    }
+    this.setState({
+      query: value,
+      page: 1,
+      images: [],
+      totalImages: 0,
+    });
+  };
+
+  loadMore = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+  };
+
   render() {
-    const { images } = this.state;
+    const { images, loading, error, totalImages } = this.state;
     return (
       <>
-        <SearchForm onSubmit={() => {}} />
-        {images.length !== 0 ? (
+        <SearchForm onSubmit={this.getQuery} />
+        {images.length !== 0 && (
           <Grid>
             {images.map(({ alt, src, id }) => {
               return (
@@ -46,10 +86,27 @@ export class Gallery extends Component {
               );
             })}
           </Grid>
-        ) : (
+        )}
+
+        {!loading && images.length === 0 && !error && (
           <Text textAlign="center">Sorry. There are no images ... 😭</Text>
         )}
-        {false && <Button type="button">Load more</Button>}
+
+        {error && <Text textAlign="center">{error}</Text>}
+
+        {!loading && totalImages !== images.length && (
+          <Button type="button" onClick={this.loadMore}>
+            Load more
+          </Button>
+        )}
+
+        {loading && <Text textAlign="center">Loading</Text>}
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          closeOnClick
+          theme="colored"
+        />
       </>
     );
   }
