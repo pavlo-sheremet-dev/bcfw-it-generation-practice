ee84b41e5e111d422f6b699fc68efffba31912c7
Iryna <aripluss@i.ua>
2023-02-15 15:04:32 +0200

    add task 4

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,38 +1,68 @@
-import { Component } from 'react';
+import { Component, useState } from 'react';
 
 import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
-export class SearchForm extends Component {
-  state = {
-    value: '',
+export const SearchForm = ({ onSubmit }) => {
+  const [value, setValue] = useState('');
+
+  const handleChange = e => {
+    setValue(e.target.value);
   };
 
-  handleChange = e => {
-    this.setState({ value: e.target.value });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    this.props.onSubmit({ ...this.state });
-    this.setState({ value: '' });
+    onSubmit({ value });
+    setValue('');
   };
 
-  render() {
-    return (
-      <SearchFormStyled onSubmit={this.handleSubmit}>
-        <FormBtn type="submit">
-          <FiSearch size="16px" />
-        </FormBtn>
-        <InputSearch
-          placeholder="What do you want to write?"
-          name="search"
-          required
-          autoFocus
-          onChange={this.handleChange}
-          value={this.state.value}
-        />
-      </SearchFormStyled>
-    );
-  }
-}
+  return (
+    <SearchFormStyled onSubmit={handleSubmit}>
+      <FormBtn type="submit">
+        <FiSearch size="16px" />
+      </FormBtn>
+      <InputSearch
+        placeholder="What do you want to write?"
+        name="search"
+        required
+        autoFocus
+        onChange={handleChange}
+        value={value}
+      />
+    </SearchFormStyled>
+  );
+};
+
+// export class SearchForm extends Component {
+//   state = {
+//     value: '',
+//   };
+
+//   handleChange = e => {
+//     this.setState({ value: e.target.value });
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     this.props.onSubmit({ ...this.state });
+//     this.setState({ value: '' });
+//   };
+
+//   render() {
+//     return (
+//       <SearchFormStyled onSubmit={this.handleSubmit}>
+//         <FormBtn type="submit">
+//           <FiSearch size="16px" />
+//         </FormBtn>
+//         <InputSearch
+//           placeholder="What do you want to write?"
+//           name="search"
+//           required
+//           autoFocus
+//           onChange={this.handleChange}
+//           value={this.state.value}
+//         />
+//       </SearchFormStyled>
+//     );
+//   }
+// }
