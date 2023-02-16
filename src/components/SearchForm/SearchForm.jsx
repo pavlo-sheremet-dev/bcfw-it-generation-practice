571bba6a2f41b182c3dfaf6abfad59eb789ece88
pavlo-sheremet-dev <goit.pavel.sheremet@gmail.com>
2023-02-16 13:16:43 +0200

    clear code

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,4 +1,4 @@
-import { Component, useState } from 'react';
+import { useState } from 'react';
 
 import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
@@ -32,37 +32,3 @@ export const SearchForm = ({ onSubmit }) => {
     </SearchFormStyled>
   );
 };
-
-// export class SearchForm extends Component {
-//   state = {
-//     value: '',
-//   };
-
-//   handleChange = e => {
-//     this.setState({ value: e.target.value });
-//   };
-
-//   handleSubmit = e => {
-//     e.preventDefault();
-//     this.props.onSubmit({ ...this.state });
-//     this.setState({ value: '' });
-//   };
-
-//   render() {
-//     return (
-//       <SearchFormStyled onSubmit={this.handleSubmit}>
-//         <FormBtn type="submit">
-//           <FiSearch size="16px" />
-//         </FormBtn>
-//         <InputSearch
-//           placeholder="What do you want to write?"
-//           name="search"
-//           required
-//           autoFocus
-//           onChange={this.handleChange}
-//           value={this.state.value}
-//         />
-//       </SearchFormStyled>
-//     );
-//   }
-// }
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -1,4 +1,4 @@
-import { Component, useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
@@ -6,110 +6,6 @@ import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 import 'react-toastify/dist/ReactToastify.css';
 
 import * as api from 'service/image-service';
-
-// const GalleryNew
-// export class Gallery extends Component {
-//   state = {
-//     images: [],
-//     page: 1,
-//     totalImages: 0,
-//     query: '',
-//     loading: false,
-//     error: '',
-//   };
-
-//   componentDidUpdate(_, prevState) {
-//     const { query, page, error } = this.state;
-//     if (prevState.query !== query || prevState.page !== page) {
-//       this.getImages();
-//     }
-//     if (prevState.error !== error && error) {
-//       toast.error(error);
-//     }
-//   }
-
-//   getImages = async () => {
-//     const { query, page } = this.state;
-//     try {
-//       this.setState({ loading: true });
-//       const { images, totalImages } = await api.getImages(query, page);
-//       if (!images.length) {
-//         this.setState({ error: 'Sorry. There are no images ... 😭' });
-//         return;
-//       }
-//       this.setState(prevState => ({
-//         images: [...prevState.images, ...images],
-//         error: '',
-//         totalImages,
-//       }));
-//     } catch (error) {
-//       this.setState({ error: 'Oops. Something went wrong 😭' });
-//     } finally {
-//       this.setState({ loading: false });
-//     }
-//   };
-
-//   getQuery = ({ value }) => {
-//     if (!value.trim() || value === this.state.query) {
-//       this.setState({ error: 'Please, change your request' });
-//       return;
-//     }
-//     this.setState({
-//       query: value,
-//       page: 1,
-//       images: [],
-//       totalImages: 0,
-//     });
-//   };
-
-//   loadMore = () => {
-//     this.setState(prevState => ({
-//       page: prevState.page + 1,
-//     }));
-//   };
-
-//   render() {
-//     const { images, loading, error, totalImages } = this.state;
-//     return (
-//       <>
-//         <SearchForm onSubmit={this.getQuery} />
-//         {images.length !== 0 && (
-//           <Grid>
-//             {images.map(({ alt, src, id }) => {
-//               return (
-//                 <GridItem key={id}>
-//                   <CardItem>
-//                     <img src={src} alt={alt} />
-//                   </CardItem>
-//                 </GridItem>
-//               );
-//             })}
-//           </Grid>
-//         )}
-
-//         {!loading && images.length === 0 && !error && (
-//           <Text textAlign="center">Sorry. There are no images ... 😭</Text>
-//         )}
-
-//         {error && <Text textAlign="center">{error}</Text>}
-
-//         {!loading && totalImages !== images.length && (
-//           <Button type="button" onClick={this.loadMore}>
-//             Load more
-//           </Button>
-//         )}
-
-//         {loading && <Text textAlign="center">Loading</Text>}
-//         <ToastContainer
-//           position="top-center"
-//           autoClose={5000}
-//           closeOnClick
-//           theme="colored"
-//         />
-//       </>
-//     );
-//   }
-// }
 
 export const Gallery = () => {
   const [images, setImages] = useState([]);
