b90e2e567e98580457157acfc454ae0f7b033f27
Lina Budkova <linabudkova35@gmail.com>
2023-02-22 15:56:16 +0200

    add task01

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Global, ThemeProvider } from '@emotion/react';
 import 'modern-normalize';
+import { store } from 'redux/store';
+import { Provider } from 'react-redux';
 
 import { App } from 'components';
 import { GlobalStyles, theme } from 'styles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <Global styles={GlobalStyles} />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Global styles={GlobalStyles} />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
